2dd94ab59bc4163296779744b7e707b7378f9a8d
Alexander Ishkin <don.shikin@gmail.com>
2014-06-05 12:55:49 -0700

    First version of function for establishing NDEx connection and user authentication using RCurl

---
+++ R/ndex_connect.r
@@ -0,0 +1,32 @@
+
+
+#' Connect to NDEx REST API
+#' 
+#' @param baseroute URL of NDEx REST server
+#' @param username character username
+#' @param password character password
+#' @return returns nothing; RCurl options object for authentication is stored in the special environment and reused with the queries
+ndex.connect <- function(username, password, baseroute='http://dev.ndexbio.org:8080/ndexbio-rest'){
+  if(missing(username) || missing(password)) stop("ndex.connect: Username or password not supplied")
+  if(grepl("/$", baseroute)) baseroute <- sub("/$", "", baseroute)
+  
+  ##Attempt authentication
+  auth_response <- getURL(paste0(baseroute, "/users/authenticate/", username, "/", password))
+  try(auth_response <- fromJSON(auth_response), silent = TRUE)
+  if(is.list(auth_response)){
+    ##Authentication successful (JSON with user data was returned)
+    ndex.opts <- curlOptions(userpwd=paste0(username, ":", password), httpauth = 1L)
+    if(!exists('NDEx.env')) NDEx.env <<- new.env(hash=T, parent=globalenv())
+    assign('ndex.opts', value=ndex.opts, envir=NDEx.env)
+    cat("Connected (user ID ", auth_response$id, ")\n",  sep='')
+  } else{
+    stop(paste("ndex.connect:", auth_response))
+  }
+  invisible(TRUE)
+}
+
+#' Check status of connection to NDEx REST server
+#' @return logical (TRUE if connection is active, FALSE otherwise)
+ndex.alive <- function(){
+  
+}
