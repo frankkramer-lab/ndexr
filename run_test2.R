82b7bdc0c35aaa971016bbb84863c3f17e446a7f
Dexter Pratt <dexterpratt@Dexters-iMac.local>
2014-11-17 16:15:57 -0800

    Updated run_test2.R
    Now provokes two errors, one in ndex.find.networks and one in
    ndex.property.graph.as.ndexgraph

--- run_test2.R
+++ run_test2.R
@@ -6,9 +6,19 @@ ndex.connect('drh', 'drh', "http://test.ndexbio.org/rest")
 ##ndex.alive()
 
 ##Find networks
-# nets <- ndex.find.networks("")
+nets <- ndex.find.networks("")
+#print(nets)
 
-net <- ndex.get.network.summary("b5c2061b-5af0-11e4-bac2-000c29873918", TRUE)
-cat(net)
+# Get a network summary for the Hox_NVC4 network
+summary <- ndex.get.network.summary("b5c2061b-5af0-11e4-bac2-000c29873918")
+#print(summary)
+
+# Search the large corpus for the neighborhood around MAPK1 and MAP2K1 
+# should include connections between the two - MAPK1 is a target of the kinase MAP2K1 
+neighborhood.property.graph <- ndex.get.neighborhood.as.property.graph("1ada3330-45cc-11e4-a9e5-000c29873918", searchString="MAPK1 MAP2K1")
+
+neighborhood.ndexgraph <- ndex.property.graph.as.ndexgraph(neighborhood.property.graph)
+
+print(neighborhood.ndexgraph)
 
 
