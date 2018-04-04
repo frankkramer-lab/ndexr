669109a46532a99d436c11188a64ea5baecac245
Florian J. Auer <florian.auer@med.uni-goettingen.de>
2018-04-04 20:36:16 +0200

    FIX: tests crashed because of missing network; updated used UUID to new version of the public one from ndextutorials

--- tests/testthat/helper_ndex_user.r
+++ tests/testthat/helper_ndex_user.r
@@ -19,6 +19,7 @@
 ##  devtools::test(filter='05_*')
 ################################################################################
 ndexTestConf=list(    user='testacc', password='testacc',             # username and password to test the server connection
-            uuidPublicNetwork='9ed0cd55-9ac0-11e4-9499-000c29202374',    # UUID of a public network (usually from "ndextutorials") to perform some read-only tests
+            #uuidPublicNetwork='9ed0cd55-9ac0-11e4-9499-000c29202374',    # doesn't work anymore April 2018
+            uuidPublicNetwork='c9243cce-2d32-11e8-b939-0ac135e8bacf',    # UUID of a public network (usually from "ndextutorials") to perform some read-only tests
             uuidPrivateNetwork='9f1ca938-0e1f-11e7-ab16-0ac135e8bacf',    # UUID of a private network (owned by the defined user) to perform some (network changing) tests on
             uuidGroup='ac4cb95c-08c8-11e7-aba2-0ac135e8bacf')    # UUID of a (private) group, to that the specified user belongs
